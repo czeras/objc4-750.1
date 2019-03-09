a918d646e795be98cc72ec1dd0df1ed998c460e6
chaomeng <custom_mail@163.com>
2019-03-09 21:32:44 +0800

    自己编译的objc4 750.1源码

---
+++ objc4-750/CZTest/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  CZTest
+//
+//  Created by czera on 2019/3/9.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        NSObject *objc = [NSObject alloc];
+        
+        
+    }
+    return 0;
+}
